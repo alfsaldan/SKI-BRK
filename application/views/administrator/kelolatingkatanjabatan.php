206e4b303f9a802d9f64328b4d6c99385bd5c617
unknown <saldanputra46@gmail.com>
2025-10-08 09:22:12 +0700

    perbaikan tambahan total di download excel di pegawai dan bug profil di tingkatan jabatan

--- application/views/administrator/kelolatingkatanjabatan.php
+++ application/views/administrator/kelolatingkatanjabatan.php
@@ -168,6 +168,8 @@
     </div>
 </div>
 
+<!-- JS Dependencies -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <!-- Datatables JS -->
